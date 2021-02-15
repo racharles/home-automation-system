9be41c690ea511124da44bcabf641fe45fd987ff
racharles <33537491+racharles@users.noreply.github.com>
2021-02-15 11:25:37 -0700

    animated plot

--- plot.py
+++ plot.py
@@ -1,35 +1,51 @@
 # Plot temperature data in a timeseries using MatPlotLib
 
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import datetime
 
 
 x = []
 y = []
 
-# open and parse file
-f = open("zetta-server/data.txt", "r")
-contents = f.read()
-lines = contents.split("\n")
-for line in lines:
-    if line:
-        parts = line.split()
-        # print(parts)
-        date = parts[0]
-        time = parts[1]
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
 
-        # datetime.time
-        temp = parts[2]
-        x.append(time)
-        y.append(float(temp))
+def animate(i, x, y):
 
-f.close()
+    # open and parse file
+    f = open("zetta-server/data.txt", "r")
+    contents = f.read()
+    lines = contents.split("\n")
+    for line in lines:
+        if line:
+            parts = line.split()
+            # print(parts)
+            date = parts[0]
+            time = parts[1]
 
-plt.plot(x,y)
-plt.xlabel('Time')
-plt.ylabel('Temperature (C)')
-plt.title("Temperature vs Time")
+            # datetime.time
+            temp = parts[2]
+            x.append(time)
+            y.append(float(temp))
 
-while(1):
-    plt.pause(0.05)
+    f.close()
+
+    # limit to 20 values
+    x = x[-20:]
+    y = y[-20:]
+
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(x, y)
+
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('Temperature over Time')
+    plt.ylabel('Temperature (deg C)')
+
+
+# Set up plot to call animate() function periodically
+ani = animation.FuncAnimation(fig, animate, fargs=(x, y), interval=1000)
 plt.show()
